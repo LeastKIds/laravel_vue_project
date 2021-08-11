0ccb41588300607b9b7f3bd5c48121c4fbc79478
leastkids <leastkids@gmail.com>
2021-08-11 23:17:15 +0900

    sidbar function and logo create

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,55 +1,38 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-container style="height : 100%;" fluid class="vContainer">
+      <Sidebar class="app"/>
+      <v-main class="app" id="main">
+        <router-view/>
+      </v-main>
+    </v-container>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <router-view/>
-    </v-main>
   </v-app>
 </template>
 
 <script>
+import Sidebar from './components/Sidebar';
 
 export default {
   name: 'App',
-
   data: () => ({
-    //
+
   }),
+  components: {
+    // eslint-disable-next-line vue/no-unused-components
+    Sidebar
+  }
+
 };
 </script>
+<style scoped>
+  .app {
+    float : left;
+  }
+  .vContainer {
+    padding : 0;
+  }
+  #main {
+    margin-left : 10px;
+  }
+</style>
---
+++ vue/src/components/Sidebar.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-container class="sidebar blue-grey darken-2">
+    <v-layout column align-center>
+      <v-flex>
+        <template>
+          <v-card
+              class="mx-auto"
+              max-width="344"
+          >
+            <v-img
+                :src="require(`@/assets/img/logo/logo.png`)"
+                :to="{ path: '/'}"
+            ></v-img>
+
+            <v-card-title>
+              Top western road trips
+            </v-card-title>
+
+            <v-card-subtitle>
+              1,000 miles of wonder
+            </v-card-subtitle>
+
+
+          </v-card>
+        </template>
+      </v-flex>
+      <v-flex>
+        test
+      </v-flex>
+    </v-layout>
+  </v-container>
+
+</template>
+
+<script>
+export default {
+  data: () => ({
+    show: false,
+  }),
+}
+</script>
+
+<style scoped>
+  .sidebar {
+    width : 250px;
+    height : 100%;
+  }
+</style>
