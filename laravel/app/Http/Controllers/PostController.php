ee95d132d9923d3397c4dee54f8728fb049ab7fe
leastkids <leastkids@gmail.com>
2021-08-14 14:45:24 +0900

    backend post store function

---
+++ laravel/app/Http/Controllers/PostController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class PostController extends Controller
+{
+    //
+    public function store(Request $request){
+        $title = $request['title'];
+        $content = $request['content'];
+        $user_id = auth()->user()['id'];
+
+        $validator = Validator::make(
+            array(
+                'title' => $title,
+                'content' => $content,
+            ),
+            array(
+                'title' => 'required|string',
+                'content' => 'required|string',
+            )
+        );
+
+        if($validator->fails()) {
+            $message = $validator->messages();
+            return response($message,422);
+        }
+
+        $post = Post::create([
+            'title' => $title,
+            'content' => $content,
+            'user_id' => $user_id
+        ]);
+
+        $data = ['success' => 1];
+        return response() -> json($data);
+    }
+}
